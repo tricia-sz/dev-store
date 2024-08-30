f96cb994670f4660f0cfd8d022a55de3c2e48300
Patricia Souza <patricia.sz.br@gmail.com>
2024-08-30 15:51:26 -0300

    Header Component

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,9 +1,9 @@
-import Header from "@/components/header/header";
+import { Header } from "@/components/header/header";
 import { ReactNode } from "react";
 export default function StoreLayout({children}: {children: ReactNode}) {
     return(
-      <div>
-        <Header />
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
+       <Header />
         {children}
       </div>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt" className={inter.variable}>
-      <body >{children}</body>
-    </html>
+    <html className={inter.variable} lang="pt">
+    <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
+  </html>
   )
 }
